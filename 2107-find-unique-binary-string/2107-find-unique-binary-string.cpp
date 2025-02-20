fc00d6b0cc028c7cf83bf3cf71eb56fe555609d9
what_a_draag <124334496+tanmay-37@users.noreply.github.com>
2025-02-20 23:01:13 +0530

    Time: 2 ms (28.82%), Space: 12.5 MB (84%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+         int n = nums.size();
+        string result = "";
+        for (int i = 0; i < n; i++) {
+            if(nums[i][i]=='0'){
+            result +='1' ;
+            }
+            else{
+                result+='0';
+            }
+        }
+        return result;
+    }
+};
