77d29182ceff1135607b0df461ec0778a3654073
what_a_draag <124334496+tanmay-37@users.noreply.github.com>
2025-05-14 23:08:55 +0530

    Time: 0 ms (100%), Space: 13.5 MB (17.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -12,13 +12,16 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* temp = head;  
-        ListNode* prev = NULL;  
-        while(temp != NULL){  
-            ListNode* front = temp->next;  
-            temp->next = prev;  
-            prev = temp;  
-            temp = front; 
-        }
-        return prev;  
+   
+   // Initialize pointer 'prev' to NULL,
+   // representing the previous node
+   ListNode* prev = NULL;  
+   while(temp != NULL){  
+       ListNode* front = temp->next;  
+       temp->next = prev;  
+       prev = temp;  
+       temp = front; 
+   }
+   return prev;  
     }
 };
